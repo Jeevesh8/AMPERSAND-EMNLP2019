58294be668d92387452a5770842d4e4640c1a891
Jeevesh Juneja <creativityinczenyoga@gmail.com>
2021-02-26 17:10:59 +0530

    Changed mnli labels to argument mining ones('O' , 'C', 'P')

--- argmining/examples/run_classifier.py
+++ argmining/examples/run_classifier.py
@@ -182,7 +182,7 @@ class MnliProcessor(DataProcessor):
 
     def get_labels(self):
         """See base class."""
-        return ["contradiction", "entailment", "neutral"]
+        return ["O", "C", "P"]
 
     def _create_examples(self, lines, set_type):
         """Creates examples for the training and dev sets."""
